5e1d564a26c727a9faad75beb345a07683016e37
SkyPark <mpark4@uchicago.edu>
2023-01-12 02:20:21 +0000

    test batch

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -27,11 +27,11 @@ module FinalProject
       g.test_framework nil
       g.factory_bot false
       g.scaffold_stylesheet false
-      g.stylesheets     false
-      g.javascripts     false
-      g.helper          false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
     end
-    
+
     config.action_controller.default_protect_from_forgery = false
     config.active_record.belongs_to_required_by_default = false
 
@@ -42,5 +42,11 @@ module FinalProject
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    Rails.application.config.middleware.insert_after(
+      ActionDispatch::Static,
+      ActionDispatch::Static,
+      Rails.root.join("docs").to_s,
+      index: config.public_file_server.index_name, headers: config.public_file_server.headers || {},
+    )
   end
 end
